d20c594dc7d23250e8d910f90b92910b40b15529
JerryU <jerry.uusitalo@gmail.com>
2024-01-19 12:45:58 +0200

    Complete 3.6

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Helsinki University Full Stack open Part 3
 
+## [Exercises 3.1-3.6](https://fullstackopen.com/osa3/node_js_ja_express#tehtavat-3-1-3-6)
+
 - 3.1: puhelinluettelon backend step1 ✅
 - 3.2: puhelinluettelon backend step2 ✅
 - 3.2: puhelinluettelon backend step3 ✅
 - 3.3: puhelinluettelon backend step3 ✅
 - 3.4: puhelinluettelon backend step4 ✅
 - 3.5: puhelinluettelon backend step5 ✅
+- 3.6: puhelinluettelon backend step6 ✅
--- index.js
+++ index.js
@@ -7,39 +7,39 @@ const app = express();
 app.use(express.json())
 
 const persons = [
-    { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
 ]
 
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
 })
 
 app.get('/info', (request, response) => {
-    const date = new Date().toString();
-    const message = 
+  const date = new Date().toString();
+  const message =
     `<p>The phonebook has info for ${persons.length} people</p>
     <p>${date}</p>`
-    response.send(message);
+  response.send(message);
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -65,11 +65,26 @@ app.post('/api/persons', (request, response) => {
   // Creates a random integer between persons length and 30
   const generateId = Math.floor(Math.random() * (30 - persons.length + 1)) + persons.length;
   person.id = generateId;
-  response.json(person)
+
+  const nameExists = persons.some(person => person.name === request.body.name)
+
+  if (!person.name || !person.number) {
+    return response.status(404).json({
+      error: 'missing name or numbber'
+    })
+  }
+  if (nameExists) {
+    return response.status(404).json({
+      error: 'name already exists in phonebook'
+    })
+  }
+  else {
+    return response.json(person)
+  }
 })
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
